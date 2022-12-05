8579b2155f78b87b72751d2a5f43db68cf9abe9c
blazmrak <blaz.mrak@kaldi.si>
2022-12-05 23:02:42 +0100

    Changed project structure

--- implementations/node/express/index.mjs
+++ implementations/node/express/index.mjs
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 app.post('/years/:year/days/:day/parts/:part', async ({params: {year, day, part}, body: {input}}, res) => {
-    const result = await (await import(`../node/years/${year}/day-${day}/part-${part}.mjs`)).execute(input)
+    const result = await (await import(`../core/years/${year}/day-${day}/part-${part}.mjs`)).execute(input)
     res.json({result})
 })
 app.listen(3000, () => console.log('listening'))
