314dee372ce3d25e5e2f4d4ac5488cb439878100
blazmrak <blaz.mrak@kaldi.si>
2022-12-07 21:01:53 +0100

    Day 7 refactor

--- implementations/rust/src/files/mod.rs
+++ implementations/rust/src/files/mod.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+#[allow(unused)]
 pub fn read_input(year: i16, day: i8, part: i8, tipe: String) -> String {
     return fs::read_to_string(format!("{year}/problems/day-{day}/test/part-{part}/{tipe}/input.txt")).expect("Should read the file");
 }
